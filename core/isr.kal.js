4c92ad3c88d3e01939b0d1d7da4f1a7575ac4bde
wolfan <wolfan@qq.com>
2018-09-21 13:31:18 +0800

    excel db

---
+++ core/isr.kal.js
@@ -0,0 +1,20 @@
+// 伊斯兰历计算
+
+const jaro='';
+const nuna=[];
+
+
+function isr(y,m,d){
+
+  let unua= 622;
+  let dif= 32;
+
+  function jaro(int){
+    let sub= int -622;
+    let sup= int +622;
+    
+    let to= Math.floor(sub+(sub/32));
+    let in= Math.floor(sub-(int/33));
+  }
+
+}
