fbe7cdb4e6a037c2aa5b05fe316d46568a39d746
wolfan <wolfan@qq.com>
2019-05-05 17:12:42 +0800

    db 0505

---
+++ core/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+nst a= 31;
+const ar= [];
+for(let i=1; i<=a; i++){ar.push(i)}
--- core/year.js
+++ core/year.js
@@ -4,10 +4,23 @@
 const jaroDek=(int)=>{return (int %2 >0)? '年':'岁'}
 
 // 公历闰年判断
-const superJaro=(int)=>{return (int %4 == 0&&int %100 !== 0)||(int %100 == 0&&int %400 == 0)? true:false}
+// const leap= (y %100)? ((y %4)? 28:29):29;
+function leapYear(int){
+  return (int %100)? ((int %4)? 28:29) :29;
+}
 
+// 获取指定月份有多少天
+function goDay(yy,mm){
+  return new Date(yy,mm,0).getDate();
+}
+// 获取指定日期星期几
+function goWeek(yy,mm,dd){
+  return new Date(`${yy}-${mm}-${dd}`).getDay();
+}
 
 // 农历闰年
 
 
 // 公元前后判断
+
+// dome's
